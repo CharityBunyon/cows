f5a8aca4c896a4496621ec52985b18126afef29c
Charity Bunyon <cseymou5@gmail.com>
2019-10-26 14:04:30 -0500

    added functions

---
+++ src/javascripts/components/cowList/cowList.js
@@ -0,0 +1,16 @@
+import cows from '../../helpers/data/cows';
+
+import './cowList.scss';
+
+const buildCows = () => {
+  // console.log('inside cow builder');
+  cows.getCows()
+    .then((response) => {
+      console.log('it worked!', response.data);
+    })
+    .catch((error) => {
+      console.error('shit broke', error);
+    });
+};
+
+export default { buildCows };
