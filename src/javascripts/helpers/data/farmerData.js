9b14753aae81a1ec75aad690dd0affd6a4154094
Charity Bunyon <cseymou5@gmail.com>
2019-10-28 19:53:14 -0500

    added getFarmers promise

---
+++ src/javascripts/helpers/data/farmerData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const getFarmers = () => new Promise((resolve, reject) => {
+  axios.get('../../../../db/farmers.json')
+    .then((response) => {
+      const demFarmers = response.data.farmers;
+      const farmers = [];
+      Object.keys(demFarmers).forEach((farmerId) => {
+        demFarmers[farmerId].id = farmerId;
+        farmers.push(demFarmers[farmerId]);
+      });
+      resolve(farmers);
+      // resolve(response.data.farmers); (1)
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getFarmers };
